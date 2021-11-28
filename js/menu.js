e56351a20fef252510207ef6c19d26187ad51d0a
AnatoliiWorkmail <jp.komod.su@gmail.com>
2021-11-28 06:45:22 +0200

    add-mob-menu

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,42 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-menu-open]'),
-    closeModalBtn: document.querySelector('[data-menu-close]'),
-    modal: document.querySelector('[data-menu]'),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden-menu');
+    refs.menu.classList.toggle('is-open');
   }
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-menu-open]'),
+//     closeModalBtn: document.querySelector('[data-menu-close]'),
+//     modal: document.querySelector('[data-menu]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden-menu');
+//   }
+// })();
+
+
